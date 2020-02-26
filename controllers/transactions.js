1b5140f8a4770c1621b7f229f97ce22235fcb8c5
Himanshu Purohit <himanshu1203@gmail.com>
2020-02-26 18:07:30 +0530

    client and init server

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,16 +1,31 @@
+const Transaction = require("../models/Transaction");
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
 
-exports.getTransactions = (req, res, next) => {
-  res.send("GET Transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error"
+    });
+  }
 };
 
 // @desc Add transaction
 // @route POST /api/v1/transactions
 // @access Public
 
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST Transaction");
 };
 
@@ -18,6 +33,6 @@ exports.addTransaction = (req, res, next) => {
 // @route DELETE /api/v1/transactions/:id
 // @access Public
 
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE Transactions");
 };

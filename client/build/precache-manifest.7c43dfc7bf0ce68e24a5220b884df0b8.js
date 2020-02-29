5f0954daa882f5f19fb14ca042ef8dd5eb5633a6
Himanshu Purohit <himanshu1203@gmail.com>
2020-02-29 12:58:37 +0530

    final

---
+++ client/build/precache-manifest.7c43dfc7bf0ce68e24a5220b884df0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772f0160f256e6e58d02ad582486e51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40f6d13781a3fa8fec2c",
+    "url": "/static/css/main.b04ea1e8.chunk.css"
+  },
+  {
+    "revision": "e246b9c90721d6131a0f",
+    "url": "/static/js/2.32d635e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.32d635e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f6d13781a3fa8fec2c",
+    "url": "/static/js/main.22d9fc33.chunk.js"
+  },
+  {
+    "revision": "3c7f62721c30427bb531",
+    "url": "/static/js/runtime-main.7ce61b84.js"
+  }
+]);
